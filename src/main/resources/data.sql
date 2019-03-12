05c3fcc3fdf584d2ef454487ccc9507af396e7f6
Gulrez <gulfarooqui1@gmail.com>
2019-03-12 17:14:11 +0530

    SpringDataRest added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into alien values(1,'Gulrez','Java');
+insert into alien values(2,'Farooqui','Python');
+insert into alien values(3,'Monu','C');
+insert into alien values(4,'Sonu','C++');
+insert into alien values(5,'Surkhab','Java');
+insert into alien values(6,'Nilofar','ML');
